c3dbef6475d1ff1d507d8ec6d909515a38e68e5f
GitBurkeD <45375222+GitBurkeD@users.noreply.github.com>
2023-02-06 15:53:37 -0800

    updated to alter constructor property per chat gpt recommendation

--- DynamicTable.js
+++ DynamicTable.js
@@ -1,63 +1,61 @@
 import { html, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
 export class DynamicTable extends LitElement {
+static getMetaConfig() {
+return {
+controlName: 'Dynamic Table',
+fallbackDisableSubmit: false,
+version: '1.0',
+iconUrl: 'one-line-text',
+groupName: 'groups',
+properties: {
+tableData: {
+type: 'string',
+title: 'Table Data',
+description: 'Data in JSON format'
+}
+},
+standardProperties: {
+readOnly: true,
+required: false,
+description: true,
+}
+};
+}
+static properties = {
+name: 'Hello',
+title: 'Hello',
+tableData: '[]'
+}
+constructor() {
+super();
+this.tableData = '[]';
+}
 
-  static getMetaConfig() {
-    return {
-      controlName: 'Dynamic Table',
-      fallbackDisableSubmit: false,
-      version: '1.0',
-      iconUrl: 'one-line-text',
-      groupName: 'groups',
-      properties: {
-        data: {
-          type: 'string',
-          title: 'Table Data',
-          description: 'Data in JSON format'
-        }
-      },
-      standardProperties: {
-        readOnly: true,
-        required: false,
-        description: true,
-      }
-    };
-  }
-  static properties = {
-    name: 'Hello',
-    title: 'Hello',
-    data: '[]'
-  }
-  constructor() {
-    super();
-    this.data = '[]';
-  }
-
-  render() {
-    const data = JSON.parse(this.data);
-    const headers = Object.keys(data[0]);
-
-    return html`
-      <table>
-        <thead>
+render() {
+const tableData = JSON.parse(this.tableData);
+const headers = Object.keys(tableData[0]);
+return html`
+  <table>
+    <thead>
+      <tr>
+        ${headers.map(header => html`<th>${header}</th>`)}
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      ${tableData.map(
+        row => html`
           <tr>
-            ${headers.map(header => html`<th>${header}</th>`)}
-            <th></th>
+            ${headers.map(header => html`<td>${row[header]}</td>`)}
+            <td><input type="checkbox" /></td>
           </tr>
-        </thead>
-        <tbody>
-          ${data.map(
-            row => html`
-              <tr>
-                ${headers.map(header => html`<td>${row[header]}</td>`)}
-                <td><input type="checkbox" /></td>
-              </tr>
-            `
-          )}
-        </tbody>
-      </table>
-    `;
-  }
+        `
+      )}
+    </tbody>
+  </table>
+`;
+}
 }
 
 const elementName = 'dynamic-table';
