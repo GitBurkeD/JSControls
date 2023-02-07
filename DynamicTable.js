2b090aa861621eb0b62cbf321558ac0f7fa0f823
GitBurkeD <45375222+GitBurkeD@users.noreply.github.com>
2023-02-06 16:09:42 -0800

    changing variable name to see if resolves issue

--- DynamicTable.js
+++ DynamicTable.js
@@ -1,61 +1,61 @@
 import { html, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
 export class DynamicTable extends LitElement {
-static getMetaConfig() {
-return {
-controlName: 'Dynamic Table',
-fallbackDisableSubmit: false,
-version: '1.0',
-iconUrl: 'one-line-text',
-groupName: 'groups',
-properties: {
-tableData: {
-type: 'string',
-title: 'Table Data',
-description: 'Data in JSON format'
-}
-},
-standardProperties: {
-readOnly: true,
-required: false,
-description: true,
-}
-};
-}
-static properties = {
-name: 'Hello',
-title: 'Hello',
-tableData: '[]'
-}
-constructor() {
-super();
-this.tableData = '[]';
-}
 
-render() {
-const tableData = JSON.parse(this.tableData);
-const headers = Object.keys(tableData[0]);
-return html`
-  <table>
-    <thead>
-      <tr>
-        ${headers.map(header => html`<th>${header}</th>`)}
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      ${tableData.map(
-        row => html`
+  static getMetaConfig() {
+    return {
+      controlName: 'Dynamic Table',
+      fallbackDisableSubmit: false,
+      version: '1.0',
+      iconUrl: 'one-line-text',
+      groupName: 'groups',
+      properties: {
+        who: {
+          type: 'string',
+          title: 'Table Data',
+          description: 'Data in JSON format'
+        }
+      },
+      standardProperties: {
+        readOnly: true,
+        required: false,
+        description: true,
+      }
+    };
+  }
+  static properties = {
+    who: {type: String}
+  }
+  constructor() {
+    super();
+    this.who = '';
+  }
+
+  render() {
+    const who = JSON.parse(this.who);
+    const headers = Object.keys(who[0]);
+
+    return html`
+      <table>
+        <thead>
           <tr>
-            ${headers.map(header => html`<td>${row[header]}</td>`)}
-            <td><input type="checkbox" /></td>
+            ${headers.map(header => html`<th>${header}</th>`)}
+            <th></th>
           </tr>
-        `
-      )}
-    </tbody>
-  </table>
-`;
-}
+        </thead>
+        <tbody>
+          ${who.map(
+            row => html`
+              <tr>
+                ${headers.map(header => html`<td>${row[header]}</td>`)}
+                <td><input type="checkbox" /></td>
+              </tr>
+            `
+          )}
+        </tbody>
+      </table>
+    `;
+  }
 }
 
 const elementName = 'dynamic-table';
