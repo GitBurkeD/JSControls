85d45f81840f6bb9ca70669e300a841037b76d97
GitBurkeD <45375222+GitBurkeD@users.noreply.github.com>
2023-02-13 13:35:35 -0800

    v1

--- timePicker.js
+++ timePicker.js
@@ -1,5 +1,5 @@
 import { html, LitElement } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
-import flatpickr from 'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.js';
+import flatpickr from 'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.js';
 
 export class TimeInputControl extends LitElement {
   static getMetaConfig() {
