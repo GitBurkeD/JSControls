c4fb474d6bc4ba7790e294cad3230a2d9324b46e
GitBurkeD <45375222+GitBurkeD@users.noreply.github.com>
2023-02-21 10:17:00 -0800

    first draft language

---
+++ language.js
@@ -0,0 +1,54 @@
+import { LitElement, html } from 'lit';
+
+export class LanguageControl extends LitElement {
+  static properties = {
+      languages: { type: Array },
+      selectedLanguage: { type: String },
+  };
+
+  static getMetaConfig() {
+    return {
+      controlName: 'Set Form Language',
+      fallbackDisableSubmit: false,
+      version: '1.0',
+      properties: {
+        language: {
+            type: 'string',
+            title: 'Language',
+            description: 'Language for form'
+        }
+      },
+      standardProperties: {
+        fieldLabel: true,
+        readOnly: true,
+        required: true,
+        description: true,
+        toolTip: true,
+        visibility: true,
+        placeholder: true
+      }
+    };
+  }
+  
+  constructor() {
+    super();
+    this.language = 'en';
+    this.selectedLanguage = this.language;
+  }
+
+  render() {
+    return html`
+      <label for="language-input">Language:</label>
+      <input type="text" id="language-input" value="${this.language}" @input="${this.handleChange}">
+    `;
+  }
+
+  handleChange(event) {
+    this.language = event.target.value;
+    document.documentElement.lang = this.language;
+  }
+}
+
+
+const elementName = 'language-control';
+customElements.define('language-control', LanguageControl);
